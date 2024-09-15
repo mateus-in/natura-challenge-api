ca1b827fb6b0a5bf8a8eb64f23a516dc66cf1af2
Mateus Castro <mateuslopescastro@outlook.com>
2024-09-15 01:09:30 -0300

    refactor: correct typo in user role verification middleware

--- src/presentation/middlewares/verify-user-role-middleware.ts
+++ src/presentation/middlewares/verify-user-role-middleware.ts
@@ -2,11 +2,11 @@ import { FastifyRequest, FastifyReply } from 'fastify'
 
 import { UserRole } from '@/domain/enums'
 
-export function verifyUserRole(allowerdRoles: UserRole[]) {
+export function verifyUserRole(allowedRoles: UserRole[]) {
   return async (request: FastifyRequest, reply: FastifyReply) => {
     const { role } = request.user
 
-    if (!allowerdRoles.includes(role)) {
+    if (!allowedRoles.includes(role)) {
       return reply.status(401).send({ error: 'Unauthorized' })
     }
   }
